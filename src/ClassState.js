00470298be1977381aafc820b329bbf1f2ca1cb7
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-04-16 17:35:51 -0500

    Efectos y métodos del ciclo de vida

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Loading } from './Loading';
 
 // Componente creado con clases
 
@@ -12,7 +13,41 @@ class ClassState extends React.Component {
         this.state = {
             // Estado de error para la prueba del código de seguridad
             error: false,
+            // Estado de error para generar efecto de carga
+            loading: false,
         };
+    }
+
+    // metodos del ciclo de vida
+
+    // Se ejecuta antes de renderizar el componente la primera vez
+    //componentWillMount() {
+    // UNSAFE_componentWillMount() {
+    //     console.log("Component Will Mount")
+    // }
+    
+    // componentDidMount() {
+    //     console.log("componentDidMount")
+    // }
+
+    // se ejecuta cada que se actualiza el estado 
+    componentDidUpdate(){
+        console.log("Actualización")
+
+        // Valida si loading es true
+
+        if(!!this.state.loading) {
+            // Recibe dos argumentos la funcion y el intervalo de cafa cuanto s ejecuta la misma
+            setTimeout(() => {
+                console.log('Empezando validación')
+
+                // Actualiza el estado
+
+                this.setState({ loading: false });
+
+                console.log('Terminando validación')
+            }, 3000);
+        }
     }
 
     render() {
@@ -27,6 +62,12 @@ class ClassState extends React.Component {
                 <p>Error: El código es incorrecto</p>
                 )}
 
+                
+                {this.state.loading && (
+                    // Componente loading
+                    <Loading />
+                )}
+
                 <input placeholder="Código de seguridad"/>
                 <button
                     // se utiliza la función "setSate" que viene del metodo "React.Compoent"
@@ -34,7 +75,8 @@ class ClassState extends React.Component {
                     // Se indica que estado actualizar enviando un objeto
                     onClick={() => 
                         // Actualizar el estado
-                        this.setState({ error: !this.state.error})
+                        // this.setState({ error: !this.state.error})
+                        this.setState({ loading: true})
                     }
                 >Comprobar</button>
             </div>
---
+++ src/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Componente creado con clases
+
+class Loading extends React.Component {
+    // 
+    // componentWillUnmount() {
+    //     console.log("componentWillUnmount")
+    // }
+
+    render() {
+        return(
+            <p>Cargando... </p>
+        );
+    }
+}
+
+export { Loading }
